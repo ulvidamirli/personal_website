862a7eb896bcbafde5a845270ecc586355adbf78
Ulvi Damirli <u.u.damirli@gmail.com>
2023-06-12 18:06:01 +0400

    Update sitemap.ts

--- react-nextjs/src/app/sitemap.ts
+++ react-nextjs/src/app/sitemap.ts
@@ -1,8 +1,6 @@
 import type { MetadataRoute } from "next";
 import { getPosts } from "@/lib/api";
 
-export const dynamic = 'force-dynamic';
-
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts = await getPosts(10000, 0);
 
