dc792b26b527d0a73d11778f9dc6d1ed2f327b81
Ulvi Damirli <u.u.damirli@gmail.com>
2023-06-12 16:38:17 +0400

    Update sitemap.ts

--- react-nextjs/src/app/sitemap.ts
+++ react-nextjs/src/app/sitemap.ts
@@ -6,17 +6,17 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   const baseSitemap = [
     {
-      url: "https://www.ulvidamirli.com",
+      url: `${process.env.WEB_CLIENT_BASE_URL}`,
       lastModified: new Date(),
     },
     {
-      url: "https://www.ulvidamirli.com/posts",
+      url: `${process.env.WEB_CLIENT_BASE_URL}/posts`,
       lastModified: new Date(),
     },
   ];
 
   const postSitemap = posts.data.map((post) => ({
-    url: `https://www.ulvidamirli.com/posts/${post.id}/${post.slug}`,
+    url: `${process.env.WEB_CLIENT_BASE_URL}/posts/${post.id}/${post.slug}`,
     lastModified: new Date(post.date_updated ?? post.date_created),
   }));
 
