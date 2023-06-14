39faaf3a3a4d037973e1f14f025d636a282f7c71
Ulvi Damirli <u.u.damirli@gmail.com>
2023-06-14 09:14:52 +0400

    Update tailwind.config.js

--- react-nextjs/tailwind.config.js
+++ react-nextjs/tailwind.config.js
@@ -4,6 +4,8 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
